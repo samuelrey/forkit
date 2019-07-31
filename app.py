181664a3bad2a879674edc2b35da8858292b04fc
samuelrey <samuelrey010@gmail.com>
2019-07-31 00:04:14 -0700

    Begin defining CLI by adding arguments
    The forkit module provides the functionality that we would like to
    achieve. This change begins to define how we use forkit.
    
    To use forkit, we require that you provide the owner's Github login
    (samuelrey) and the name of the repository (forkit).
    
    This change does not connect the module to the CLI. This change does not
    contain the complete CLI to come.
    
    Test plan:
    From the main project directory, run these commands:
    `./app.py -h` -- to see the description of the program and arguments
    `./app.py` -- to see that the program fails unless both arguments are
    provided
    `./app.py samuelrey forkit` -- to see that the program succeeds silently

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3.7
+import argparse
 
 if __name__ == '__main__':
-    print('hello world')
+    parser = argparse.ArgumentParser(description='Start contributing sooner. '
+                                     'Use forkit to get your dev environment '
+                                     'setup.')
+    parser.add_argument('owner_login',
+                        help='The Github login of the repo owner, eg. '
+                        'samuelrey')
+    parser.add_argument('repo_name',
+                        help='The name of the Github repository to fork, eg. '
+                        'forkit')
+    args = parser.parse_args()
+
